81c74e21a385921800f46f4acd63faba546975d6
jackyzha0 <j.zhao2k19@gmail.com>
2021-01-16 08:41:42 -0800

    pdf token chunking

--- pdf.js
+++ pdf.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
 
-let dataBuffer = fs.readFileSync('/Users/ansonyu/Projects/summarization/testing/test.pdf');
+let dataBuffer = fs.readFileSync('./testing/test-senior.pdf');
 
+// turn arr into 'chunks' of chunkSize
+function chunk(arr, chunkSize) {
+	var res = [];
+	for (var i = 0, len = arr.length; i < len; i += chunkSize)
+		res.push(arr.slice(i, i + chunkSize));
+	return res;
+}
+
+
+const MAX_TOK_SIZE = 4096
+const TOK_SCALE = 2
 pdf(dataBuffer).then(function(data) {
-
 	cleanedText = data.text.replace(/(\r\n|\n|\r)/gm,"")
-	console.log(cleanedText); 
+	tokens = cleanedText.split(" ")
+	chunks = chunk(tokens, Math.round(MAX_TOK_SIZE / TOK_SCALE)).map(c => c.join(" "))
+	console.log(chunks);
+	console.log(chunks.length) 
         
 });
